20e63229dc9ae12a82f28cbd77bbb94cc2f42344
Raphael Arnaldo Cruz <53103201+INN0C0DE@users.noreply.github.com>
2022-07-05 16:58:30 +0800

    Add files via upload

---
+++ Activity5/Activity5/Form1.Designer.vb
@@ -0,0 +1,240 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.rb1 = New System.Windows.Forms.RadioButton()
+        Me.rb2 = New System.Windows.Forms.RadioButton()
+        Me.rb3 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.cb1 = New System.Windows.Forms.CheckBox()
+        Me.cb2 = New System.Windows.Forms.CheckBox()
+        Me.cb3 = New System.Windows.Forms.CheckBox()
+        Me.cb4 = New System.Windows.Forms.CheckBox()
+        Me.cb5 = New System.Windows.Forms.CheckBox()
+        Me.cb6 = New System.Windows.Forms.CheckBox()
+        Me.cb7 = New System.Windows.Forms.CheckBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tb1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'rb1
+        '
+        Me.rb1.AutoSize = True
+        Me.rb1.Font = New System.Drawing.Font("Poppins", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rb1.Location = New System.Drawing.Point(19, 35)
+        Me.rb1.Name = "rb1"
+        Me.rb1.Size = New System.Drawing.Size(80, 30)
+        Me.rb1.TabIndex = 0
+        Me.rb1.Text = "Deluxe"
+        Me.rb1.UseVisualStyleBackColor = True
+        '
+        'rb2
+        '
+        Me.rb2.AutoSize = True
+        Me.rb2.Font = New System.Drawing.Font("Poppins", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rb2.Location = New System.Drawing.Point(19, 71)
+        Me.rb2.Name = "rb2"
+        Me.rb2.Size = New System.Drawing.Size(85, 30)
+        Me.rb2.TabIndex = 1
+        Me.rb2.Text = "Special"
+        Me.rb2.UseVisualStyleBackColor = True
+        '
+        'rb3
+        '
+        Me.rb3.AutoSize = True
+        Me.rb3.Font = New System.Drawing.Font("Poppins", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rb3.Location = New System.Drawing.Point(19, 107)
+        Me.rb3.Name = "rb3"
+        Me.rb3.Size = New System.Drawing.Size(74, 30)
+        Me.rb3.TabIndex = 2
+        Me.rb3.Text = "Primo"
+        Me.rb3.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.rb3)
+        Me.GroupBox1.Controls.Add(Me.rb2)
+        Me.GroupBox1.Controls.Add(Me.rb1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Poppins", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(30, 27)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(145, 167)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Pizza"
+        '
+        'cb1
+        '
+        Me.cb1.AutoSize = True
+        Me.cb1.Font = New System.Drawing.Font("Poppins", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cb1.Location = New System.Drawing.Point(36, 29)
+        Me.cb1.Name = "cb1"
+        Me.cb1.Size = New System.Drawing.Size(88, 30)
+        Me.cb1.TabIndex = 4
+        Me.cb1.Text = "Cheese"
+        Me.cb1.UseVisualStyleBackColor = True
+        '
+        'cb2
+        '
+        Me.cb2.AutoSize = True
+        Me.cb2.Font = New System.Drawing.Font("Poppins", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cb2.Location = New System.Drawing.Point(36, 65)
+        Me.cb2.Name = "cb2"
+        Me.cb2.Size = New System.Drawing.Size(84, 30)
+        Me.cb2.TabIndex = 5
+        Me.cb2.Text = "Pepper"
+        Me.cb2.UseVisualStyleBackColor = True
+        '
+        'cb3
+        '
+        Me.cb3.AutoSize = True
+        Me.cb3.Font = New System.Drawing.Font("Poppins", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cb3.Location = New System.Drawing.Point(36, 101)
+        Me.cb3.Name = "cb3"
+        Me.cb3.Size = New System.Drawing.Size(118, 30)
+        Me.cb3.TabIndex = 6
+        Me.cb3.Text = "Bacon Ham"
+        Me.cb3.UseVisualStyleBackColor = True
+        '
+        'cb4
+        '
+        Me.cb4.AutoSize = True
+        Me.cb4.Font = New System.Drawing.Font("Poppins", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cb4.Location = New System.Drawing.Point(36, 137)
+        Me.cb4.Name = "cb4"
+        Me.cb4.Size = New System.Drawing.Size(113, 30)
+        Me.cb4.TabIndex = 7
+        Me.cb4.Text = "Mushroom"
+        Me.cb4.UseVisualStyleBackColor = True
+        '
+        'cb5
+        '
+        Me.cb5.AutoSize = True
+        Me.cb5.Font = New System.Drawing.Font("Poppins", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cb5.Location = New System.Drawing.Point(36, 173)
+        Me.cb5.Name = "cb5"
+        Me.cb5.Size = New System.Drawing.Size(119, 30)
+        Me.cb5.TabIndex = 8
+        Me.cb5.Text = "Onion Chilli"
+        Me.cb5.UseVisualStyleBackColor = True
+        '
+        'cb6
+        '
+        Me.cb6.AutoSize = True
+        Me.cb6.Font = New System.Drawing.Font("Poppins", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cb6.Location = New System.Drawing.Point(36, 209)
+        Me.cb6.Name = "cb6"
+        Me.cb6.Size = New System.Drawing.Size(153, 30)
+        Me.cb6.TabIndex = 9
+        Me.cb6.Text = "Tomato P-Apple"
+        Me.cb6.UseVisualStyleBackColor = True
+        '
+        'cb7
+        '
+        Me.cb7.AutoSize = True
+        Me.cb7.Font = New System.Drawing.Font("Poppins", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cb7.Location = New System.Drawing.Point(36, 245)
+        Me.cb7.Name = "cb7"
+        Me.cb7.Size = New System.Drawing.Size(83, 30)
+        Me.cb7.TabIndex = 10
+        Me.cb7.Text = "Salami"
+        Me.cb7.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.cb7)
+        Me.GroupBox2.Controls.Add(Me.cb6)
+        Me.GroupBox2.Controls.Add(Me.cb5)
+        Me.GroupBox2.Controls.Add(Me.cb4)
+        Me.GroupBox2.Controls.Add(Me.cb3)
+        Me.GroupBox2.Controls.Add(Me.cb2)
+        Me.GroupBox2.Controls.Add(Me.cb1)
+        Me.GroupBox2.Font = New System.Drawing.Font("Poppins", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(221, 27)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(204, 284)
+        Me.GroupBox2.TabIndex = 11
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Ingredients"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Poppins", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(25, 225)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 26)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Price:"
+        '
+        'tb1
+        '
+        Me.tb1.Font = New System.Drawing.Font("Poppins", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tb1.Location = New System.Drawing.Point(31, 256)
+        Me.tb1.Name = "tb1"
+        Me.tb1.Size = New System.Drawing.Size(143, 30)
+        Me.tb1.TabIndex = 13
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(435, 323)
+        Me.Controls.Add(Me.tb1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Activity 5"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents rb1 As System.Windows.Forms.RadioButton
+    Friend WithEvents rb2 As System.Windows.Forms.RadioButton
+    Friend WithEvents rb3 As System.Windows.Forms.RadioButton
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents cb1 As System.Windows.Forms.CheckBox
+    Friend WithEvents cb2 As System.Windows.Forms.CheckBox
+    Friend WithEvents cb3 As System.Windows.Forms.CheckBox
+    Friend WithEvents cb4 As System.Windows.Forms.CheckBox
+    Friend WithEvents cb5 As System.Windows.Forms.CheckBox
+    Friend WithEvents cb6 As System.Windows.Forms.CheckBox
+    Friend WithEvents cb7 As System.Windows.Forms.CheckBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents tb1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ Activity5/Activity5/Form1.vb
@@ -0,0 +1,54 @@
+﻿Public Class Form1
+
+  
+    Private Sub CheckBox4_CheckedChanged(sender As Object, e As EventArgs) Handles cb4.CheckedChanged
+
+    End Sub
+
+    Private Sub rb1_CheckedChanged(sender As Object, e As EventArgs) Handles rb1.CheckedChanged
+        If rb1.Checked Then
+            cb1.Checked = True
+            cb2.Checked = False
+            cb3.Checked = True
+            cb4.Checked = False
+            cb5.Checked = True
+            cb6.Checked = False
+            cb7.Checked = False
+            tb1.Text = "185"
+
+        End If
+    End Sub
+
+    Private Sub rb2_CheckedChanged(sender As Object, e As EventArgs) Handles rb2.CheckedChanged
+        If rb2.Checked Then
+            cb1.Checked = True
+            cb2.Checked = True
+            cb3.Checked = True
+            cb4.Checked = True
+            cb5.Checked = True
+            cb6.Checked = False
+            cb7.Checked = False
+            tb1.Text = "250"
+
+        End If
+    End Sub
+
+    Private Sub rb3_CheckedChanged(sender As Object, e As EventArgs) Handles rb3.CheckedChanged
+        If rb3.Checked Then
+            cb1.Checked = True
+            cb2.Checked = True
+            cb3.Checked = True
+            cb4.Checked = True
+            cb5.Checked = True
+            cb6.Checked = True
+            cb7.Checked = True
+            tb1.Text = "290"
+
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tb1.Enabled = False
+
+    End Sub
+End Class
